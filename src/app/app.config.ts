df920a24fbf386bae4371ba2b158293c8848fb55
Ruan Failache <ruanfailache@gmail.com>
2024-01-20 19:24:44 -0300

    refactor: adjust svg

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,26 @@
 import { ApplicationConfig } from "@angular/core";
 import { provideRouter } from "@angular/router";
+import { provideSvgIconsConfig } from "@ngneat/svg-icon";
 
 import { routes } from "./app.routes";
+import { baseIcons } from "./svg/base";
+import { illustrationsIcons } from "./svg/illustrations";
 
 export const appConfig: ApplicationConfig = {
-	providers: [provideRouter(routes)],
+	providers: [
+		provideRouter(routes),
+		provideSvgIconsConfig({
+			sizes: {
+				xxs: "4px",
+				xs: "8px",
+				sm: "12px",
+				md: "16px",
+				lg: "20px",
+				xl: "24px",
+				xxl: "32px",
+			},
+			defaultSize: "md",
+			icons: [...baseIcons, ...illustrationsIcons],
+		}),
+	],
 };
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,12 +1,13 @@
 import { CommonModule } from "@angular/common";
 import { NgModule } from "@angular/core";
+import { SvgIconComponent } from "@ngneat/svg-icon";
 
 import { ButtonComponent } from "./components/button/button.component";
 import { DropdownMenuComponent } from "./components/dropdown-menu/dropdown-menu.component";
 
 @NgModule({
-	imports: [CommonModule],
+	imports: [CommonModule, SvgIconComponent],
 	declarations: [ButtonComponent, DropdownMenuComponent],
-	exports: [ButtonComponent, DropdownMenuComponent],
+	exports: [ButtonComponent, DropdownMenuComponent, SvgIconComponent],
 })
 export class SharedModule {}
